c4cb0a02729503798dc7953239d2893af9a6c83c
Ammar <113832578+Ammar-Barakat@users.noreply.github.com>
2024-12-02 18:14:59 +0200

    Add User Authentication

---
+++ bookhouseAPI/bookhouseAPI/DTOs/Authentication/LoginDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bookhouseAPI.DTOs.Authentication
+{
+    public class LoginDTO
+    {
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
